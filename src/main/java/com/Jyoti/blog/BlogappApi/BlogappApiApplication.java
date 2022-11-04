10435ce45e635823784de72a1980f673e933b3e1
chetryJyoti <jyotichetry087@gmail.com>
2022-11-04 21:37:42 +0530

    added server started statement

--- src/main/java/com/Jyoti/blog/BlogappApi/BlogappApiApplication.java
+++ src/main/java/com/Jyoti/blog/BlogappApi/BlogappApiApplication.java
@@ -8,6 +8,7 @@ public class BlogappApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogappApiApplication.class, args);
+		System.out.println("Server started");
 	}
 
 }
