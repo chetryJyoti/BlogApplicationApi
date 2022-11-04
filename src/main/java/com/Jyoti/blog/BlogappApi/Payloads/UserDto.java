b2089252229a25d206424e8be5629ce1b4cc150a
chetryJyoti <jyotichetry087@gmail.com>
2022-11-05 01:45:45 +0530

    restricting the users from inserting invalid data to the database

--- src/main/java/com/Jyoti/blog/BlogappApi/Payloads/UserDto.java
+++ src/main/java/com/Jyoti/blog/BlogappApi/Payloads/UserDto.java
@@ -4,13 +4,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @NoArgsConstructor
 @Getter
 @Setter
 public class UserDto {
     private int id;
+
+    @NotEmpty
+    @Size(min = 4,message = "Username must be minimum 4 characters long")
     private String name;
+
+    @Email(message = "Email is invalid")
     private String email;
+
+    @NotEmpty
+    @Size(min = 4,max = 10,message = "password must be min 3 and max 10 char long")
     private String password;
+
+    @NotEmpty
     private String about;
 }
