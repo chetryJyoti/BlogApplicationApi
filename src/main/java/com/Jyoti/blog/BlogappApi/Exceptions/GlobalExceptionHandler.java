42783f608fd1414a4bc6a68ee5bea197f9fa3727
chetryJyoti <jyotichetry087@gmail.com>
2022-11-05 01:48:56 +0530

    handling MethodNotAllowedException and showing custom msg to the user

--- src/main/java/com/Jyoti/blog/BlogappApi/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/Jyoti/blog/BlogappApi/Exceptions/GlobalExceptionHandler.java
@@ -4,15 +4,34 @@ package com.Jyoti.blog.BlogappApi.Exceptions;
 import com.Jyoti.blog.BlogappApi.Payloads.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    //handling resource not found exception
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex){
         String msg = ex.getMessage();
         ApiResponse apiResponse = new ApiResponse(msg,false);
         return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+
+    //handling invalid data exception
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> methodArgumentNotValidException(MethodArgumentNotValidException ex){
+        Map<String ,String> resp = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error)->{
+            String fieldName = ((FieldError)error).getField();
+            String msg = error.getDefaultMessage();
+            resp.put(fieldName,msg);
+        });
+        return new ResponseEntity<Map<String,String>>(resp,HttpStatus.BAD_REQUEST);
     }
 }
