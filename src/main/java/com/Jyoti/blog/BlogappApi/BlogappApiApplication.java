4c17f2c803b31f3e39d4e510e552fa11404731dc
chetryJyoti <jyotichetry087@gmail.com>
2022-11-05 00:45:04 +0530

    created modelmapper bean

--- src/main/java/com/Jyoti/blog/BlogappApi/BlogappApiApplication.java
+++ src/main/java/com/Jyoti/blog/BlogappApi/BlogappApiApplication.java
@@ -1,7 +1,9 @@
 package com.Jyoti.blog.BlogappApi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogappApiApplication {
@@ -9,6 +11,11 @@ public class BlogappApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogappApiApplication.class, args);
 		System.out.println("Server started");
+	}
+
+	@Bean
+	public ModelMapper modalMapper(){
+		return new ModelMapper();
 	}
 
 }
