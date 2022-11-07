aad5c37587c73a8137a680e24f7971de8dc685c4
chetryJyoti <jyotichetry087@gmail.com>
2022-11-07 21:41:48 +0530

    added global constants file for managing all constants in the api

---
+++ src/main/java/com/Jyoti/blog/BlogappApi/Config/AppConstants.java
@@ -0,0 +1,8 @@
+package com.Jyoti.blog.BlogappApi.Config;
+
+//config file for handling all constants in this application
+public class AppConstants {
+    public static final String PAGE_NUMBER="0";
+    public static final String PAGE_SIZE = "10";
+    public static final String SORT_BY = "postId";
+}
